1f3354a924290016e408034d3e4fa304a1641721
TasTake <tasov98@gmail.com>
2022-11-05 18:41:28 +0600

    Added more understandable error messages

--- test_main_page.py
+++ test_main_page.py
@@ -8,7 +8,8 @@ from selenium.webdriver.support import expected_conditions as EC
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    page.open() 
+    page.should_be_login_link()                     # открываем страницу
+    #page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
     #use "pytest -v --tb=line --language=en test_main_page.py" to launch
